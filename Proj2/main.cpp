6997d8ea027bd50c818520eb0d0bc4a69d4f6b54
Robert Lowe <robertmlowe@rmlowe.com>
2024-07-11 20:02:17 +0100

    281. Building and Running C++ Programs with VSCode on Mac OSX

---
+++ Proj2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int fav_number;
+    cout << "Hello from Proj2" << endl;
+    cout << "Enter your favotite number: ";
+    cin >> fav_number;
+    cout << "Your favorite number is " << fav_number << endl;
+    return 0;
+}
