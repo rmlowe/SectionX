75536881b40eb37483e45931f98d71a71adeb744
Robert Lowe <robertmlowe@rmlowe.com>
2024-07-11 20:25:08 +0100

    282. Debugging C++ Programs with VSCode on Mac

--- Proj2/main.cpp
+++ Proj2/main.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-    int fav_number;
+    int fav_number{100};
     cout << "Hello from Proj2" << endl;
-    cout << "Enter your favotite number: ";
+    cout << "Enter your favorite number: ";
     cin >> fav_number;
     cout << "Your favorite number is " << fav_number << endl;
     return 0;
